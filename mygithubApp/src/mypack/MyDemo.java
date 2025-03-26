10ccb93ab8dbc1560736bb0a88eaeea44495f9f5
VEMULA SARVAGNA <sarvagnavemulaaimg@gmail.com>
2025-03-26 19:31:16 +0530

    My Second Version

---
+++ mygithubApp/src/mypack/MyDemo.java
@@ -0,0 +1,7 @@
+package mypack;
+
+public class MyDemo {
+	public static void main(String arg[]) {
+	System.out.println("hello everyone!!");
+	}
+}
--- mygithubApp/src/mypack/Welcome.java
+++ mygithubApp/src/mypack/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello welcome to github demo");
+		System.out.println("Hi welcome to github demo");
 
 	}
 
